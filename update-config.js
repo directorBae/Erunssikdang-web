4ca236c02b8ecf003ded8da8b83a9df0c9b36bb5
Jeongwon, Bae <bjw020615@gmail.com>
2024-07-17 13:28:21 +0900

    edited config

--- update-config.js
+++ update-config.js
@@ -1,13 +1,13 @@
-import fs from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
 // config.json 파일 읽기
 const configFilePath = "./src/configs/config.json";
-const config = JSON.parse(fs.readFileSync(configFilePath, "utf8"));
+const config = JSON.parse(readFileSync(configFilePath, "utf8"));
 
 // 변수 수정
 config.isDev = false;
 
 // 수정된 내용을 다시 config.json 파일에 쓰기
-fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2));
+writeFileSync(configFilePath, JSON.stringify(config, null, 2));
 
 console.log("Config file updated successfully");
